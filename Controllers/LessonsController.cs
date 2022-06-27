247e26f6a75d194d2176d470c92edaea89b2904e
Mirkopm <miroslav.pukovski@gmail.com>
2022-06-27 20:36:19 +0300

    Add project files.

---
+++ Controllers/LessonsController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyFirstWebApp.Controllers
+{
+    public class LessonsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Jokes()
+        {
+            return View();
+        }
+
+        public IActionResult Processors()
+        {
+            return View();
+        }
+
+        public IActionResult BookLibrary()
+        {
+            return View();
+        }
+    }
+}
